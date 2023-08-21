825906e1b8752a8f3776685aa2fde04fa565c09d
anber22 <51233546+anber22@users.noreply.github.com>
2023-08-21 08:46:38 +0800

    处理默认路由问题

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import '@/styles/globals.css'
-import Template from "../components/Layout/index";
+import Template from "@/components/Layout/index";
 
 export default function App({ Component, pageProps }) {
   return (
