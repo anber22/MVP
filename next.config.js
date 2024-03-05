85281ddc7779a54f6ffb423d9d2357a59bfff993
anber22 <51233546+anber22@users.noreply.github.com>
2024-03-06 00:42:14 +0800

    调整请求方式

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const nextConfig = {
   async rewrites() {
     return [
       {
+        source: '/api/:path*',
+        destination: 'https://stable-diffsuion.aiproshots.com/api/:path*'
+      },
+      {
         source: '/mvp/:path*',
         destination: 'https://back-end-api.aiproshots.com/:path*'
       },
@@ -15,21 +19,7 @@ const nextConfig = {
   },
   serverRuntimeConfig: {
     maxDuration: 300000, // 设置超时时间为60秒
-  },
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
-},
+  }
 }
 
 module.exports = nextConfig
